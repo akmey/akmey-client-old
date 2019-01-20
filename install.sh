d11ef2d735f5d8c5d2f6ae943b998382437fc500
Leon <leonekmi@hotmail.com>
2019-01-20 14:37:09 +0100

    Better install script

--- install.sh
+++ install.sh
@@ -17,25 +17,58 @@ echo "
 Akmey - Install script for Linux
 "
 
-confirm "Install Akmey client in /usr/local/bin/ [y/N]"
 
-if [[ $EUID -ne 0 ]]; then
-    tput setaf 3
-    echo "This script must be run as root" 1>&2
-    tput sgr 0
-    exit 1
+if [ $1 = "compile" ]; then
+    confirm "*Compile* Akmey client in /usr/local/bin/ [y/N]"
+     path_to_executable=$(which go)
+    if [ -x "$path_to_executable" ] ; then
+        echo "Go found: $path_to_executable"
+        if [[ $EUID -ne 0 ]]; then
+            tput setaf 3
+            echo "This script must be run as root" 1>&2
+            tput sgr 0
+            exit 1
+        else
+            if [ -f main.go ]
+            then
+                go get github.com/mattn/go-sqlite3 github.com/mitchellh/go-homedir github.com/schollz/progressbar github.com/urfave/cli
+                go build -o bin/akmey
+                chmod 755 bin/akmey
+                cp bin/akmey /usr/local/bin
+                tput setaf 2
+                echo "Enjoy keys with akmey command !"
+                tput sgr 0
+                exit 0
+            else
+                tput setaf 3
+                echo "Missing akmey source file, please clone the entire repository with git" 1>&2
+                tput sgr 0
+                exit 1
+            fi
+        fi
+    fi
 else
-    if [ -e bin/akmey ]
-    then
-        cp bin/akmey /usr/local/bin
-        tput setaf 2
-        echo "Enjoy keys with akmey command !"
-        tput sgr 0
-        exit 0
-    else
+    confirm "Install Akmey client in /usr/local/bin/ [y/N]"
+
+    if [[ $EUID -ne 0 ]]; then
         tput setaf 3
-        echo "Missing akmey bin, please clone the entire repository with `git`" 1>&2
+        echo "This script must be run as root" 1>&2
         tput sgr 0
         exit 1
+    else
+        if [ -f bin/akmey ]
+        then
+            chmod 755 bin/akmey
+            cp bin/akmey /usr/local/bin
+            tput setaf 2
+            echo "Enjoy keys with akmey command !"
+            tput sgr 0
+            exit 0
+        else
+            tput setaf 3
+            echo "Missing akmey bin, please clone the entire repository with git" 1>&2
+            tput sgr 0
+            exit 1
+        fi
     fi
 fi
