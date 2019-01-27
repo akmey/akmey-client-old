8bdae3945b12d6c7cfe1423d0b6edf87515260df
Luclu7 <luclu7@luclu7.fr>
2019-01-27 16:30:40 +0100

    only compile from the script, correct shebang

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env bash
 
 confirm() {
@@ -17,10 +16,8 @@ echo "
 Akmey - Install script for Linux
 "
 
-
-if [ $1 = "compile" ]; then
-	confirm "*Compile* Akmey client in /usr/local/bin/ [y/N]"
-	 path_to_executable=$(which go)
+confirm "*Compile* Akmey client in /usr/local/bin/ [y/N]"
+	path_to_executable=$(command -v go)
 	if [ -x "$path_to_executable" ] ; then
 		echo "Go found: $path_to_executable"
 		if [[ $EUID -ne 0 ]]; then
@@ -43,31 +40,6 @@ if [ $1 = "compile" ]; then
 				tput setaf 3
 				echo "Missing akmey source file, please clone the entire repository with git" 1>&2
 				tput sgr 0
-				exit 1
-			fi
-		fi
-	fi
-else
-	confirm "Install Akmey client in /usr/local/bin/ [y/N]"
-
-	if [[ $EUID -ne 0 ]]; then
-		tput setaf 3
-		echo "This script must be run as root" 1>&2
-		tput sgr 0
-		exit 1
-	else
-		if [ -f bin/akmey ]
-		then
-			chmod 755 bin/akmey
-			cp bin/akmey /usr/local/bin
-			tput setaf 2
-			echo "Enjoy keys with akmey command !"
-			tput sgr 0
-			exit 0
-		else
-			tput setaf 3
-			echo "Missing akmey bin, please clone the entire repository with git" 1>&2
-			tput sgr 0
 			exit 1
 		fi
 	fi
